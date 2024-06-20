6b0a0602652ac3a3cea0d8fedcb9e938004795fc
Natalia Lozano <acuarionat2004@gmail.com>
2024-06-20 12:27:07 -0400

    navegacion

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-
+import { NavLink } from 'react-router-dom'
 const Header = () => {
   return (
     <div>
         <ul>
+    <NavLink>
         <li>Home</li>
+        </NavLink>
+        <NavLink>
         <li>About</li>
+        </NavLink>
+        <NavLink>
         <li>Dashboard</li>
+        </NavLink>
         </ul>
     </div>
   )
--- src/rutas/Navigation.jsx
+++ src/rutas/Navigation.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import
+import Home from "../componentes/Home";
+import Dashboard from "../componentes/Dashboard";
+import About from "../componentes/About";
 import React from 'react'
 
 const Navigation = () => {
   return (
    <BrowserRouter>
    <Routes>
-    <Route path="/" element={<}/>
+    <Route path="/" element={<Home/>}/>
+    <Route path="/about" element={<About/>}/>
+    <Route path="/dashboard" element={<Dashboard/>}/>
    </Routes>
    </BrowserRouter>
   )
