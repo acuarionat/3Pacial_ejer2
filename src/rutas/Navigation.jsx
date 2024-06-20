db1493b71cce8bfaac7f4c81c83480c650522474
Natalia Lozano <acuarionat2004@gmail.com>
2024-06-20 12:22:41 -0400

    creacion home

---
+++ src/rutas/Navigation.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import
+import React from 'react'
+
+const Navigation = () => {
+  return (
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<}/>
+   </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default Navigation
